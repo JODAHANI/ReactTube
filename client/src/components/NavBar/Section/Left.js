6cb826052a72694db8cdd2b780997e3f44750beb
JODA HANI <dan0748@naver.com>
2022-07-06 17:28:54 +0900

    2022-07-06 commit

--- client/src/components/NavBar/Section/Left.js
+++ client/src/components/NavBar/Section/Left.js
@@ -1,18 +1,23 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faYoutube } from "@fortawesome/free-brands-svg-icons";
 import {
-    Link
-  } from "react-router-dom";
+  Link
+} from "react-router-dom";
 
 function LeftMenu() {
   return (
-    <div className='nav-left nav-items'>
-        <Link to="/"><h2>ReactTube</h2></Link>
-    </div>
+    <Link to="/">
+      <div className='nav-left nav-items' style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+        <FontAwesomeIcon icon={faYoutube} size='2x' style={{ color: '#1890ff' }} />
+        <h2 style={{ margin: 0 }}>ReactTube</h2>
+      </div>
+    </Link>
   )
 }
 
 
-export default LeftMenu 
+export default LeftMenu
 
 
-        
+
--- client/src/components/NavBar/Section/Right.js
+++ client/src/components/NavBar/Section/Right.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import axios from 'axios'
+import { useSelector } from 'react-redux';
+import { Menu} from 'antd';
+
 import {
   Link, useNavigate
 } from "react-router-dom";
 
 
+
 function RightMenu() {
+  const user = useSelector(state => state.user)
   const navigate = useNavigate()
   const logoutHandler = () => {
     axios.get('/api/logout').then(res => {
@@ -15,13 +20,23 @@ function RightMenu() {
       }
     })
   }
-  return (
-    <div className='nav-right nav-items'>
-      <Link to="/">Home</Link>
-      <Link to="/login">Login</Link>
-      <Link to="/register">Register</Link>
-      <Link to="/" onClick={logoutHandler}>Logout</Link>
-    </div>
-  )
+  if (user.userData && user.userData.isAuth) {
+    return (
+      <div className='nav-right nav-items'>
+        <Menu.Item key="cart">
+          <Link to="/">Home</Link>
+        </Menu.Item>
+        <Link to="/" onClick={logoutHandler}>Logout</Link>
+      </div>
+    )
+  } else {
+    return (
+      <div className='nav-right nav-items'>
+        <Link to="/">Home</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link>
+      </div>
+    )
+  }
 }
 export default RightMenu 
